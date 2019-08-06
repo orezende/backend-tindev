15d614575f94b312c688fc2a40de8b5a670509d5
joaorabelo98 <joaorabello99@gmail.com>
2019-08-06 20:47:02 -0300

    adicionado cors

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 
+const cors = require('cors');
+
 const server = express();
 
 mongoose.connect('mongodb+srv://root:root@tindev-h1rbs.mongodb.net/myapp?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes); 
 
